65b94958c9fa3800b732c78e6f49dc5d998fe04c
Manuel Andrés Vizone <andresvizone@gmail.com>
2025-06-07 12:12:29 -0300

    Ejemplos practicos TP Integrador de programacion 1

---
+++ TP_Integrador_Programacion1.py
@@ -0,0 +1,46 @@
+
+# Busqueda lineal
+
+def busqueda_lineal(lista, objetivo):
+    for i in range(len(lista)):
+        if lista[i] == objetivo:
+            return i
+    return -1
+
+
+# Busqueda binaria
+
+def busqueda_binaria(lista, objetivo):
+    izquierda, derecha = 0, len(lista) - 1
+    while izquierda <= derecha:
+        medio = (izquierda + derecha) // 2
+        if lista[medio] == objetivo:
+            return medio
+        elif lista[medio] < objetivo:
+            izquierda = medio + 1
+        else:
+            derecha = medio - 1
+    return -1
+
+
+# Bubble sort
+
+def bubble_sort(lista):
+    n = len(lista)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if lista[j] > lista[j+1]:
+                lista[j], lista[j+1] = lista[j+1], lista[j]
+    return lista
+
+
+# quick sort
+
+def quick_sort(lista):
+    if len(lista) <= 1:
+        return lista
+    pivote = lista[len(lista) // 2]
+    izquierda = [x for x in lista if x < pivote]
+    centro = [x for x in lista if x == pivote]
+    derecha = [x for x in lista if x > pivote]
+    return quick_sort(izquierda) + centro + quick_sort(derecha)
